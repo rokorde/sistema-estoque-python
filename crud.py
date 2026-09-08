8dda12d20e8d55865e9915c9da828c74a267ff46
Rodrigo Kord <94815130+rokorde@users.noreply.github.com>
2026-09-08 09:56:32 -0300

    feat: adicionadas funcoes para atualizar e deletar registros

--- crud.py
+++ crud.py
@@ -17,3 +17,19 @@ def listar_bicicletas():
     resultados = cursor.fetchall()
     conn.close()
     return resultados
+
+def atualizar_estoque(id_bike, nova_quantidade):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute('''
+        UPDATE bicicletas SET quantidade = ? WHERE id = ?
+    ''', (nova_quantidade, id_bike))
+    conn.commit()
+    conn.close()
+
+def deletar_bicicleta(id_bike):
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM bicicletas WHERE id = ?', (id_bike,))
+    conn.commit()
+    conn.close()
